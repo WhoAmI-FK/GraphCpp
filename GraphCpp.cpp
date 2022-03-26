448fd2e152f6da2dc7ca433f2d5bee720ff8dc06
WhoAmI-FK <Faismen2004@mail.ru>
2022-03-26 18:54:53 +0100

    Pre-final commit

--- GraphCpp.cpp
+++ GraphCpp.cpp
@@ -6,12 +6,16 @@ namespace whoamifk_graph {
         T _val;
         size_t _cost;
         adjNode* _next;
+        adjNode(T val, size_t cost, adjNode* node=nullptr)
+            :_val(val),_cost(cost),_next(node)
+        {
+        }
     };
     template<typename T>
     struct graphEdge {
         T _start_ver;
         T _end_ver;
-        int _weight;
+        size_t _weight;
     };
 
     template<typename T, class _Alloc=std::allocator<T>>
@@ -28,12 +32,26 @@ namespace whoamifk_graph {
         Graph(graphEdge<T> edges[], size_t _num_of_edges, size_t nodes)
             : _nodes(nodes)
         {
-
+            _head = new adjNode*[nodes]();
+            for (size_t i = 0; i < nodes; ++i) _head[i] = nullptr;
+            for (size_t i = 0; i < _num_of_edges; ++i) {
+                adjNode* newNode = getAdjListNode(edges[i]._end_ver, edges[i]._weight, _head[edges[i]._start_ver]);
+                _head[edges[i]._start_ver] = newNode;
+            }
+        }
+        ~Graph() {
+            for (int i = 0; i < _nodes; ++i) {
+                delete[] _head[i];
+            }
+            delete[] _head;
         }
     private:
         root _head;
         size_t _nodes;
-   
+        adjNode<T>* getAdjListNode(T value, size_t cost, adjNode<T>* head) {
+            adjNode<T>* newNode = new adjNode<T>(value,cost,head);
+            return newNode;
+        }
     };
 }
 
